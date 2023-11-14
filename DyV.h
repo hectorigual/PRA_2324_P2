a5f76b235a9cbf9ef4c6ef6d4aa23b1456fbf2fd
hectorigual <igualhector@gmail.com>
2023-11-14 09:48:01 +0100

    Añadido

--- DyV.h
+++ DyV.h
@@ -3,6 +3,94 @@
 
 using namespace std;
 
+template<typename T>
+int PartitionINI(std::vector<T>& V, int ini, int fin){
+	T x = V[ini];
+	int i = ini;
+	int j ;	
+	for(j = ini ;j < fin; j++){
+
+	       if(V[j] <= x){
+	       int aux = V[i];
+		V[i] = V[j];
+ 		V[j] = aux;
+		i = i +1;
+	       }
+	}
+	int aux1 = V[i];
+	V[i] = V[fin];
+	V[fin] = aux1;
+	return i;
+}
+template<typename T>
+void QuickSortINI(std::vector<T>& V, int ini, int fin){
+
+	if(ini < fin){
+		int pivot = PartitionINI(V, ini, fin);
+		QuickSortINI(V, ini, pivot-1);
+		QuickSortINI(V, pivot +1, fin);
+	}
+}
+
+template<typename T>
+int PartitionMED(std::vector<T>& V, int ini, int fin){
+  int medio = (ini +fin) / 2;
+  T x = V[medio];
+	int i = ini;
+	
+	int j ;	
+	for(j = ini; j<=fin; j++){
+
+	       if(V[j] < x){
+	       int aux = V[i];
+		V[i] = V[j];
+ 		V[j] = aux;
+		i = i +1;
+	       }
+	}
+	int aux1 = V[i];
+	V[i] = V[medio];
+	V[medio] = aux1;
+	return i;
+}
+template<typename T>
+void QuickSortMED(std::vector<T>& V, int ini, int fin){
+	
+	if(ini < fin){
+		int pivot = PartitionMED(V, ini, fin);
+		QuickSortMED(V, ini, pivot -1);
+		QuickSortMED(V, pivot +1, fin);
+	}
+}
+template<typename T>
+int Partition(std::vector<T>& V, int ini, int fin){
+        T x = V[fin];
+        int i = ini;
+        int j ;
+        for(j = ini ;j < fin; j++){
+
+               if(V[j] <= x){
+               int aux = V[i];
+                V[i] = V[j];
+                V[j] = aux;
+                i = i +1;
+               }
+        }
+        int aux1 = V[i];
+        V[i] = V[j];
+        V[j] = aux1;
+        return i;
+}
+
+template<typename T>
+void QuickSort(std::vector<T>& V, int ini, int fin){
+	if(ini < fin){
+		int pivot = Partition(V, ini, fin);
+		QuickSort(V, ini, pivot -1);
+		QuickSort(V, pivot +1, fin);
+	}
+}
+
 template <typename T>
 int BusquedaBinaria(T X, vector<T>& V, int ini, int fin){
         if(ini > fin){
